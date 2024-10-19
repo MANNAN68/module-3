58d5d97034ad57076a6a1448c1311b365882f8df
mannanswe <147591955+mannanswe@users.noreply.github.com>
2024-10-19 16:34:12 +0600

    update 3.3

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,9 @@ import Form from "./components/Form";
 import "./styles.css";
 
 function App() {
-  let statuses = ["empty", "typing", "submitting", "success", "error"];
   return (
     <>
-      {statuses.map((status) => (
-        <Form key={status} status={status} />
-      ))}
+      <Form />
     </>
   );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,22 +1,17 @@
 import React from "react";
 
-const Form = ({ status }) => {
+const Form = () => {
   if (status === "success") return <h1>That's right!</h1>;
   return (
     <>
       <h2>City quiz</h2>
       <p>What city is located on two continents?</p>
       <form>
-        <textarea disabled={status === "submitting"}></textarea>
+        <textarea></textarea>
         <br />
-        <button
-          id="button"
-          disabled={status === "empty" || status === "submitting"}
-        >
-          Submit
-        </button>
-        {status === "submitting" && <p id="loading">Loading...</p>}
-        {status === "error" && <p className="Error">There was an error</p>}
+        <button>Submit</button>
+        <p id="loading">Loading...</p>
+        <p className="Error">There was an error</p>
       </form>
     </>
   );
